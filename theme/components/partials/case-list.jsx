5e71126a75fdee06747b5e86d50484dbac916ee8
Leonard Martin <leonard.martin@gmail.com>
2017-09-12 11:39:01 +0100

    Filter on name attribute

--- theme/components/partials/case-list.jsx
+++ theme/components/partials/case-list.jsx
@@ -9,20 +9,16 @@ class CaseList extends Component {
     this.setState({ filter: props.filter || '' });
   }
 
-  getCaseName(c) {
-    return `${c.name} (${c.email})`;
-  }
-
   filter(e) {
     this.setState({ filter: e.target.value});
   }
 
   render(props, state) {
     const filter = (this.state.filter || '').toLowerCase();
-    const cases = (this.props.cases || []).filter(c => c.reference.toString().includes(filter) || this.getCaseName(c).toLowerCase().includes(filter));
+    const cases = (this.props.cases || []).filter(c => c.reference.toString().includes(filter) || c.name.toLowerCase().includes(filter));
     return (
       <div>
-        <input type="text" placeholder="Filter cases..." onInput={e => this.filter(e)}/>
+        <input type="search" placeholder="Filter cases..." onInput={e => this.filter(e)}/>
         { !!cases.length && (
           <table class="case-list">
             {
@@ -30,7 +26,7 @@ class CaseList extends Component {
                 return (
                   <tr class="case" onClick={() => route(`/view/${c.reference}/overview`)}>
                     <td>{c.reference}</td>
-                    <td>{this.getCaseName(c)}</td>
+                    <td>{c.name}</td>
                     <td><span class={`status status-${c.state}`}>{c.state}</span></td>
                     <td class="nowrap">
                       <span class="counter tasks" title={`${c.openTasks} open task(s)`}>
